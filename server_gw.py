e3e9a3b6c57688bdaf638d70c675e2e8bd3466cf
unknown <voicevon@gmail.com>
2020-12-28 18:03:34 +0800

    move home

--- server_gw.py
+++ server_gw.py
@@ -1,27 +1,28 @@
 
 import subprocess
+from flask import Flask, render_template, request
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    # body = '<body style="background-color:powderblue;">'
-    body = '<body style="color:white; link-color:green">'
-    body += 'Text only,  <a href="http://localhost:5000/ls">Hello World</a>'
-    return (body)
-
-@app.route('/shell_command')
-def welcome():
-   return ('welcome')
+def index():
+    # https://www.techiediaries.com/flask-tutorial-templates/
+    return render_template("index.html", message="Hello Flask!");   
+@app.route('/exec')
+def exec():
+    command = request.args.get('command')
+    return (command)
+    
+    response = subprocess.run([command], stdout=subprocess.PIPE)
+    ss = str(response.stdout, 'utf-8')
+    print(command,ss)
+    rr = ss.replace('\n','<p>')
+    return (rr)
 @app.route('/ls')
 def ls():
-    # useless_cat_call = subprocess.run(["cat"], stdout=subprocess.PIPE, text=True, input="Hello from the other side")
     response = subprocess.run(["ls","-l"], stdout=subprocess.PIPE)
-    # useless_cat_call = 'I am here'
-    xx = str(response.stdout, 'utf-8')
-    rr = xx.replace('\n','<p>')
-
+    ss = str(response.stdout, 'utf-8')
+    rr = ss.replace('\n','<p>')
     return (rr)
     
 if __name__ == '__main__':
