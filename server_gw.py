291178effa22557bdcb916020c0be9590eca125b
Xuming <voicevon@gmail.com>
2020-12-28 21:26:03 +0800

    input text box

--- server_gw.py
+++ server_gw.py
@@ -1,4 +1,4 @@
-
+ 
 import subprocess
 from flask import Flask, render_template, request
 
@@ -10,15 +10,27 @@ def index():
     return render_template("index.html", message="Hello Flask!");   
 @app.route('/exec')
 def exec():
+    # content = '<form action="/exec" method="post">'
+    # content += '<input type="text" name="command"/>'
+    # content += '<input type="submit" value="Execute">'
+    # content += '</form><hr>'
+    content = '<input type="text" name="input" length = 40>'
+    content += '<input type="button" value="Exec" onclick="location.href =' 
+    #content += "'/abc'" + '"/>'
+    next_command="ls -l"
+    content += "'/exec?command=" + next_command +"'" + '"/>'
+    content += '<p>'
+    # content = ''
     raw_command = request.args.get('command', default='')
     command = raw_command.split(' ')
-    print(command)
-    content =  raw_command 
+    #print(command)
+    # return (command)
+    content +=  raw_command 
     content += '<hr> <pre>'
 
     raw_response = subprocess.run(command, stdout=subprocess.PIPE)
     response = str(raw_response.stdout, 'utf-8')
-    print(command,response)
+    #print(command,response)
     content += response.replace('\n','<p>')
     return (content)
 
